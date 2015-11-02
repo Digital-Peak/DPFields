c644429ad1fa1a94c804147497c85420b84a7ce2
laoneo <allon.moritz@digital-peak.com>
2015-11-02 09:12:45 +0100

    Case 3085: Load the default query when not set

--- com_dpfields/admin/models/types/sql.php
+++ com_dpfields/admin/models/types/sql.php
@@ -60,6 +60,11 @@ class DPFieldsTypeSql extends DPFieldsTypeBase
 		$fieldNode->setAttribute('value_field', 'text');
 		$fieldNode->setAttribute('key_field', 'value');
 
+		if (! $fieldNode->getAttribute('query'))
+		{
+			$fieldNode->setAttribute('query', 'select id as value, name as text from #__users');
+		}
+
 		return parent::postProcessDomNode($field, $fieldNode, $form);
 	}
 }
