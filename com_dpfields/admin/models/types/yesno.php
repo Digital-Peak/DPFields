80d8875d8e8efc67dd54158c585d4e029dc91e86
laoneo <allon.moritz@digital-peak.com>
2015-11-09 08:35:13 +0100

    Case 3104: Adding the needed classes for yes no field

--- com_dpfields/admin/models/types/yesno.php
+++ com_dpfields/admin/models/types/yesno.php
@@ -14,9 +14,38 @@ class DPFieldsTypeYesno extends DPFieldsTypeList
 
 	protected function postProcessDomNode ($field, DOMElement $fieldNode, JForm $form)
 	{
+		$return = parent::postProcessDomNode($field, $fieldNode, $form);
 		$fieldNode->setAttribute('type', 'radio');
 
-		return parent::postProcessDomNode($field, $fieldNode, $form);
+		// JFormField replaces invalid characters
+		$id = 'params_' . preg_replace('#\W#', '_', $fieldNode->getAttribute('name'));
+
+		JFactory::getDocument()->addScriptDeclaration(
+				"jQuery( document ).ready(function() {
+	jQuery('#jform_" . $id . " input').bind('click', function(e)
+	{
+		jQuery('#jform_" . $id . " label').attr('class', 'btn');
+		if (jQuery('#jform_" . $id . "0')[0].checked || (!jQuery('#jform_" . $id . "0')[0].checked && !jQuery('#jform_" . $id . "1')[0].checked))
+		{
+			jQuery('#jform_" . $id . " label[for=\"jform_" . $id . "0\"]').attr('class', 'btn btn-success');
+		}
+		else
+		{
+			jQuery('#jform_" . $id . " label[for=\"jform_" . $id . "1\"]').attr('class', 'btn btn-danger');
+		}
+	});
+	jQuery('#jform_" . $id . " label').attr('class', 'btn');
+	if (jQuery('#jform_" . $id . "0')[0].checked || (!jQuery('#jform_" . $id . "0')[0].checked && !jQuery('#jform_" . $id . "1')[0].checked))
+	{
+		jQuery('#jform_" . $id . " label[for=\"jform_" . $id . "0\"]').attr('class', 'btn btn-success');
+	}
+	else
+	{
+		jQuery('#jform_" . $id . " label[for=\"jform_" . $id . "1\"]').attr('class', 'btn btn-danger');
+	}
+});");
+
+		return $return;
 	}
 
 	protected function getOptions ($field)
